3ac7e075a8af029dcc4cdeda40da7e660496a6da
Daniel-Rk <75667485+Daniel-Rk@users.noreply.github.com>
2023-02-25 01:59:55 +0100

    Add files via upload

---
+++ finance_us.py
@@ -0,0 +1,17 @@
+from openbb_terminal.sdk import openbb
+import matplotlib.pyplot as plt
+
+class OpenBB:
+    def __init__(self, ticker_array):
+        self.ticker_array = ticker_array
+        self.dataframe = dataframe
+        self.index_array = self.dataframe.columns.tolist()
+
+    def _income_statement(self):
+        data_container = []
+        for i,j in enumerate(self.ticker_array):
+            
+
+
+
+
